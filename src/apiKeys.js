6b8ca5b3b3d2ba0f8fb38b412cfabc6f070cc93e
sanjaymohansingh <115239492+sanjaymohansingh@users.noreply.github.com>
2023-05-20 07:54:40 -0700

    route commit

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "95508e1892e25c1dc1c018acca9dc104",
+  key: "",
   base: "https://api.openweathermap.org/data/2.5/",
 };
